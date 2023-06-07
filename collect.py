dd8e34bdcb54bd7a2ac865a3b8db494c53d35d73
Rafael Domiciano <rafael.domiciano@gmail.com>
2023-06-07 16:36:45 -0300

    Various fix. Add json output, add vpn collect

--- collect.py
+++ collect.py
@@ -7,15 +7,20 @@ import argparse
 import csv
 import json
 import os
-import pandas as pd
 import subprocess
 import datetime
+import pandas as pd
 import warnings
-from collections import defaultdict
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Font, PatternFill, Alignment
+from openpyxl.utils import get_column_letter
 from google.cloud import compute_v1
 from googleapiclient import discovery
 from google.cloud import monitoring_v3
+from google.auth import default as gcp_cred
 from easydict import EasyDict
+from collections import defaultdict
 
 warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
 
@@ -30,6 +35,125 @@ class Logger:
     def log(cls, level, message):
         if cls.level >= level:
             print(message)
+
+class Exporter:
+    # Save the desired outputs
+    outputs = []
+    out_dir = "output"
+
+    # To make date consistent between the outputs
+    today = datetime.datetime.today()
+    date = today.strftime('%d/%m/%y %H:%M:%S')
+ 
+    @classmethod
+    def set_outputs(cls, outputs):
+        cls.outputs = outputs
+        os.makedirs(cls.out_dir, exist_ok=True)
+
+        # Delete the file if it exists
+        if 'xls' in outputs:
+            xls_path = os.path.join(cls.out_dir, 'output.xlsx')
+            if os.path.exists(xls_path):
+                os.remove(xls_path)
+
+    @classmethod
+    def export(cls, data, resource_type):
+        Logger.log(1, f' | - Saving {resource_type}')
+
+        if len(data) == 0:
+            return
+
+        if 'csv' in cls.outputs:
+            cls.save_to_csv(data, resource_type)
+        if 'xls' in cls.outputs:
+            cls.save_to_xls(data, resource_type)
+        if 'json' in cls.outputs:
+            cls.save_to_json(data, resource_type)
+    
+    @classmethod
+    def save_to_csv(cls, data, resource_type):
+        output_path = os.path.join(cls.out_dir, f"output_{resource_type}.csv")
+        with open(output_path, 'w', newline='') as csvfile:
+            # Write a line (commented) that has some metadata info about this file
+            csvfile.write(f'# Type: {resource_type} Date: {cls.date}\n')
+            # Dump the Dict variable
+            writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+            # Write the header
+            writer.writeheader()
+            # Plot the data
+            writer.writerows(data)
+
+    @classmethod
+    def save_to_json(cls, data, resource_type):
+        json_path = os.path.join(cls.out_dir, f'output_{resource_type}.json')
+        with open(json_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def save_to_xls(cls, data, resource_type):
+        xls_path = os.path.join(cls.out_dir, 'output.xlsx')
+        
+        # Check if the file exists
+        file_exists = os.path.exists(xls_path)
+        
+        # If the file exists, load it into the workbook object
+        if file_exists:
+            workbook = load_workbook(xls_path)
+        else:
+            workbook = Workbook()
+            # Remove the default sheet created when creating a new workbook
+            # Change to have a first sheet as a resume to all the data collected
+            default_sheet = workbook.active
+            workbook.remove(default_sheet)
+
+        # Create a new sheet or get the existing sheet by name
+        if resource_type in workbook.sheetnames:
+            sheet = workbook[resource_type]
+            sheet.delete_rows(1, sheet.max_row)  # Clear existing content
+        else:
+            sheet = workbook.create_sheet(resource_type)
+
+        # Convert data to a DataFrame
+        df = pd.DataFrame(data)
+
+        # Append the DataFrame to the sheet
+        for row in dataframe_to_rows(df, index=False, header=True):
+            sheet.append(row)
+
+        # Apply bold formatting to the first row
+        first_row = sheet[1]
+        for cell in first_row:
+            cell.font = Font(bold=True)
+
+        # Adjust column width to fit the content
+        for column in sheet.columns:
+            max_length = 0
+            column_letter = column[0].column_letter
+            # Look for the cell text size, using the fixed value 40 as max
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = min(len(str(cell.value)), 40)
+                except TypeError:
+                    pass
+            # Put 20% more to make a safe width visualization
+            adjusted_width = (max_length + 2) * 1.2
+            sheet.column_dimensions[column_letter].width = adjusted_width
+
+        # Apply color scheme to the table
+        header_fill = PatternFill(start_color="BDD7EE", end_color="BDD7EE", fill_type="solid")
+        data_fill = PatternFill(start_color="F2F2F2", end_color="F2F2F2", fill_type="solid")
+        header_row = sheet[1]
+        for cell in header_row:
+            cell.fill = header_fill
+            cell.font = Font(bold=True)
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+        for row in sheet.iter_rows(min_row=2):
+            for cell in row:
+                cell.fill = data_fill
+
+        # Save the workbook
+        workbook.save(xls_path)
 
 def GetHumanReadable(size,precision=2):
     suffixes=['B','KB','MB','GB','TB']
@@ -49,13 +173,10 @@ def check_api_is_enabled(project_id, api):
     return True if response.get('state') != "DISABLED" else False
 
 def get_logged_in_project():
-    result = subprocess.run(['gcloud', 'config', 'get-value', 'project'], capture_output=True, text=True)
-    if result.returncode == 0:
-        return result.stdout.strip()
-    else:
-        raise RuntimeError(f"Failed to retrieve logged-in project: {result.stderr.strip()}")
+    credentials, project_id = gcp_cred()
+    return project_id
 
-def list_vm_instances(project_id):
+def list_vm_instances(project_id, gke_ignore):
     def get_vm_metrics(vm_name):
         # Set the time range for the metrics query (last month)
         end_time = datetime.datetime.utcnow()
@@ -141,6 +262,14 @@ def list_vm_instances(project_id):
                 if i==total:
                     print()
 
+                # Check for the goog-gke-node label
+                IS_GKE=True if 'goog-gke-node' in instance.labels else False
+
+                if gke_ignore:
+                    if IS_GKE:
+                        Logger.log(3, 'Compute is a GKE Node. Next')
+                        continue
+
                 # Get the metrics of the vm, to calculate avg and p95 cpu usage
                 metric_data = get_vm_metrics(instance.name)
                 avg_cpu_usage = calculate_average_usage(metric_data)
@@ -159,9 +288,6 @@ def list_vm_instances(project_id):
                     contains_windows = any("WINDOWS" in feature.type for feature in disk.guest_os_features)
                     if contains_windows:
                         SO="Windows"
-
-                # Check for the goog-gke-node label
-                IS_GKE=True if 'goog-gke-node' in instance.labels else False
 
                 # Check if preemptible
                 PREEMPTIBLE=False
@@ -415,11 +541,12 @@ def list_artifact_registry_repos(project_id):
         Logger.log(2, f' |- Found {len(resp.repositories)}')
 
         for rep in resp.repositories:
-            print(rep)
+            #print(rep)
 
             sizeB=0
             if 'sizeBytes' in rep:
                 sizeB = rep.sizeBytes
+            size = GetHumanReadable(sizeb)
 
             parts=rep.name.split('/')
             name=parts[-1]
@@ -429,7 +556,8 @@ def list_artifact_registry_repos(project_id):
                 'name': name,
                 'location': location.locationId,
                 'format': rep.format,
-                'sizeBytes': sizeB
+                'sizeBytes': sizeB,
+                'size': size
             }
             repositories.append(repository_data)
 
@@ -469,23 +597,47 @@ def list_networks(project_id):
     networks = {}
     networks['net'] = []
     networks['peer'] = []
+    networks['vpn'] = []
 
     if not check_api_is_enabled(project_id,'compute'):
         Logger.log (2, "  | Compute API is not enabled")
         return networks
     
+    # https://cloud.google.com/compute/docs/reference/rest/v1
     client = discovery.build('compute', 'v1')
+
+    # Collect VPN data
+    # https://cloud.google.com/compute/docs/reference/rest/v1/vpnTunnels/aggregatedList
+    Logger.log(2, 'Searching for VPN')
+
+    r_vpn_agg = client.vpnTunnels().aggregatedList(project=project_id).execute()
+    for name, vpn_tunnels_scoped_list in r_vpn_agg['items'].items():
+        vpn_resp=EasyDict(vpn_tunnels_scoped_list)
+
+        if 'vpnTunnels' in vpn_resp:
+            for vpn in vpn_resp.vpnTunnels:
+                Logger.log(3, f'Found VPN {vpn.name}')
+                vpn_data = {
+                    'project': project_id,
+                    'region': vpn.region.split('/')[-1],
+                    'name': vpn.name,
+                    'status': vpn.status,
+                    'description': vpn.description
+                }
+                networks['vpn'].append(vpn_data)
+
+    # https://cloud.google.com/compute/docs/reference/rest/v1/networks/list
     raw_resp = client.networks().list(project=project_id).execute()
     vpc_resp = EasyDict(raw_resp)
 
     for vpc in vpc_resp.items:
-        #print(vpc)
         network_data = {
             'project': project_id,
             'name': vpc.name
         }
         networks['net'].append(network_data)
 
+        # Collect peering information
         if 'peerings' in vpc:
             for peer in vpc.peerings:
                 peer_data = {
@@ -497,43 +649,13 @@ def list_networks(project_id):
 
     return networks
 
-def save_to_json(data, resource_type, output_directory):
-    if len(data) == 0:
-        return
-
-    output_path = os.path.join(output_directory, f"output_{resource_type}.json")
-    with open(output_path, "w") as json_file:
-        json.dump(data, json_file)
-
-def save_to_csv(data, resource_type, output_directory):
-    if len(data) == 0:
-        #Logger.log(2, f'{resource_type} with no data')
-        return
-
-    output_path = os.path.join(output_directory, f"output_{resource_type}.csv")
-    with open(output_path, 'w', newline='') as csvfile:
-        # Write a line (commented) that has some metadata info about this file
-        today = datetime.datetime.today()
-        date=today.strftime('%d/%m/%y %H:%M:%S')
-        csvfile.write(f'# Type: {resource_type} Date: {date}\n')
-
-        # Dump the Dict variable
-        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
-        # Write the header
-        writer.writeheader()
-        # Plot the data
-        writer.writerows(data)
-
-def save_to_xls(data, resource_type, writer):
-    df = pd.DataFrame(data)
-    df.to_excel(writer, sheet_name=resource_type, index=False)
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--file', help='File with a list of GCP projects')
-    parser.add_argument('-o', '--output', action='append', dest='output', choices=['csv', 'xls'], default=['csv'], help='Type of output (csv or xls) (can be used multiple times)')
+    parser.add_argument('-o', '--output', action='append', dest='output', choices=['csv', 'json', 'xls'], default=['csv'], help='Type of output (csv or xls) (can be used multiple times)')
     parser.add_argument('-l', '--log-level', choices=['1', '2', '3'], default='1', help='Log level (1: normal, 2: verbose, 3: debug)')
     parser.add_argument('-r', action='append', dest='options', default=[], help='Specify a resource (compute, sql, functions, gcs, gke, art_repo, pubsub) (can be used multiple times)')
+    parser.add_argument('-g', '--gke-ignore', action='store_true', help='Ignore GKE nodes in the compute collect')
     args = parser.parse_args()
 
     Logger.set_level (int(args.log_level))
@@ -548,6 +670,7 @@ def main():
         Logger.log(2, f"Going to use the already logged-in project {projects}")
 
     # Create the variables that will be used further
+    all_projects = []
     all_instances = []
     all_cloudsql = []
     all_functions = []
@@ -556,21 +679,20 @@ def main():
     all_artifact_repos = []
     all_pubsub_topics = []  
     all_networks = {}
-    all_networks['net'] = []
-    all_networks['peer'] = []
+
+    all_projects = list(map(lambda project_id: {'project': project_id}, projects))
 
     for project in projects:
         Logger.log(1, f"** Processing project: {project} **")
 
-        # Network VPC and subnets
+        # Network VPC, VPN, Peerings
         if not args.options or 'network' in args.options:
             networks = list_networks(project)
-            all_networks['net'].extend(networks['net'])
-            all_networks['peer'].extend(networks['peer'])
+            all_networks = networks
 
         # Compute Machines
         if not args.options or 'compute' in args.options:
-            instances = list_vm_instances(project)
+            instances = list_vm_instances(project, args.gke_ignore)
             all_instances.extend(instances)
         
         # CloudSQL
@@ -604,35 +726,20 @@ def main():
             all_pubsub_topics.extend(pubsub_topics)
 
     Logger.log(1, "Saving to output")
-    output_directory = 'output'
-    os.makedirs(output_directory, exist_ok=True)
 
-    if 'csv' in args.output:
-        Logger.log(1, " |- Saving to csv")
-
-        save_to_csv(all_networks['net'], 'network', output_directory)
-        save_to_csv(all_networks['peer'], 'network_peer', output_directory)
-        save_to_csv(all_instances, 'compute', output_directory)
-        save_to_csv(all_cloudsql, 'cloudsql', output_directory)
-        save_to_csv(all_functions, 'functions', output_directory)
-        save_to_csv(all_gcs_buckets, 'gcs', output_directory)
-        save_to_csv(all_gke_clusters, 'gke', output_directory)
-        save_to_csv(all_artifact_repos, 'artifact', output_directory)
-        save_to_csv(all_pubsub_topics, 'pubsub', output_directory)
-    if 'xls' in args.output:
-        Logger.log(1, " |- Saving to xls")
-
-        xls_path = os.path.join(output_directory, 'output.xlsx')
-        with pd.ExcelWriter(xls_path) as writer:
-            save_to_xls(all_networks['net'], 'network', writer)
-            save_to_xls(all_networks['peer'], 'network_peer', writer)
-            save_to_xls(all_instances, 'compute', writer)
-            save_to_xls(all_cloudsql, 'cloudsql', writer)
-            save_to_xls(all_functions, 'functions', writer)
-            save_to_xls(all_gcs_buckets, 'gcs', writer)
-            save_to_xls(all_gke_clusters, 'gke', writer)
-            save_to_xls(all_artifact_repos, 'artifact', writer)
-            save_to_xls(all_pubsub_topics, 'pubsub', writer)
+    # Export the collected data to the desired outputs
+    Exporter.set_outputs(args.output)
+    Exporter.export(all_networks['net'], 'network')
+    Exporter.export(all_networks['peer'], 'net_peer')
+    Exporter.export(all_networks['vpn'], 'net_vpn')
+    Exporter.export(all_instances, 'compute')
+    Exporter.export(all_cloudsql, 'cloudsql')
+    Exporter.export(all_functions, 'functions')
+    Exporter.export(all_gcs_buckets, 'gcs')
+    Exporter.export(all_gke_clusters, 'gke')
+    Exporter.export(all_artifact_repos, 'artifact')
+    Exporter.export(all_pubsub_topics, 'pubsub')
+    Exporter.export(all_projects, 'projects')
 
     Logger.log(1, 'Finished')
 
